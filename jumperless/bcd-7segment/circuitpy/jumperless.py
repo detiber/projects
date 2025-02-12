bb8f096030f097ab648acdba6dc9853c7ca8a591
Jason DeTiberus <detiber@users.noreply.github.com>
2025-02-11 19:38:08 -0500

    auto-config success!

--- jumperless/bcd-7segment/circuitpy/jumperless.py
+++ jumperless/bcd-7segment/circuitpy/jumperless.py
@@ -14,6 +14,11 @@
 
 import busio
 
+EOT = b'\x04'
+ACK = b'\x06'
+STX = b'\x02'
+RDY = b'\x01'
+
 class Jumperless:
     def __init__(self, rx_pin, tx_pin, baudrate=115200):
         self.uart = busio.UART(tx_pin, rx_pin, baudrate=baudrate)
@@ -24,7 +29,43 @@ class Jumperless:
         self.connections.append(connection)
 
     def make_connections(self):
-        str_list = [f"{str(x)}-{str(y)}" for (x, y) in self.connections]
-        command = f"f {",".join(str_list)},\n"
-        print(command.encode('utf-8'))
-        self.uart.write(command.encode('utf-8'))
+        # the transmission order is:
+        # Arduino (A) - Jumperless (J)
+        # 
+        # each step waits for the next acknowledgement
+
+        # A -> J : STX (\x02)
+        # A <- J : RDY (\x01)
+        # A -> J : sends one bridge (trailing comma)  6-9,
+        # A <- J : ACK (\x06)
+        # A -> J : sends another bridge (trailing comma)  4-20,
+        # A <- J : ACK (\x06)
+        # repeat until all bridges are sent
+        # A -> J : EOT (\x04)
+
+        # clear the input buffer
+        print("Clearing the input buffer")
+        self.uart.reset_input_buffer()
+
+        print("Sending STX")
+        self.uart.write(STX)
+        resp = self.uart.read(1)
+        if resp != RDY:
+            print(f"received a response other than RDY: {resp}")
+            return
+
+        for (source,dest) in self.connections:
+            connection = f"{str(source)}-{str(dest)},"
+            print(f"Sending connection: {connection}")
+            self.uart.write(connection)
+            resp = self.uart.read(1)
+            if resp != ACK:
+                print(f"received a response other than ACK: {resp}")
+                return
+            
+        print("sending EOT")
+        self.uart.write(EOT)
+        # str_list = [f"{str(x)}-{str(y)}" for (x, y) in self.connections]
+        # command = f"f {",".join(str_list)},\n"
+        # print(command.encode('utf-8'))
+        # self.uart.write(command.encode('utf-8'))
